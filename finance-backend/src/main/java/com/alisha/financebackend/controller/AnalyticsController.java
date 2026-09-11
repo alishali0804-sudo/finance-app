09c2901eef10ce15f422926d042ff059e2c76f7e
Alisha Li <alii0064@student.monash.edu>
2026-09-12 01:33:06 +1000

    Connect dashboard to analytics backend

--- finance-backend/src/main/java/com/alisha/financebackend/controller/AnalyticsController.java
+++ finance-backend/src/main/java/com/alisha/financebackend/controller/AnalyticsController.java
@@ -1,6 +1,7 @@
 package com.alisha.financebackend.controller;
 
 import com.alisha.financebackend.service.AnalyticsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/analytics")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AnalyticsController {
 
     private final AnalyticsService analyticsService;
